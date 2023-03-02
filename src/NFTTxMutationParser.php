e3a81afa612d986e5a16f7f1653c1027049a39b0
ZGrguric <zgrguric@xrplwin.com>
2023-03-02 23:26:26 +0100

    [WIP] Mint tx

--- src/NFTTxMutationParser.php
+++ src/NFTTxMutationParser.php
@@ -1,14 +1,91 @@
 <?php declare(strict_types=1);
 
 namespace XRPLWin\XRPLNFTTxMutatationParser;
-#use XRPLWin\XRPL\Utilities\BalanceChanges;
-#use Brick\Math\BigDecimal;
-#use XRPL_PHP\Core\Utilities as XRPLPHPUtilities;
 
 /**
  * NFT Transaction Mutation Parser
  */
 class NFTTxMutationParser
 {
+  private readonly string $account;
+  private readonly \stdClass $tx;
+  private array $result_in = [];
+  private array $result_out = [];
 
+  public function __construct(string $reference_account, \stdClass $tx)
+  {
+    $this->account = $reference_account;
+    $this->tx = $tx;
+
+    $meta = $this->tx->meta;
+
+    if(!isset($meta->AffectedNodes)) {
+      return;
+    }
+
+    $affected_account = $this->tx->Account;
+    if($this->tx->TransactionType = 'NFTokenMint' && isset($this->tx->Issuer)) {
+      $affected_account = $this->tx->Issuer;
+    }
+
+    if($affected_account != $this->account) {
+      return;
+    }
+    //dd($affected_account);
+
+    
+    $in = $out = [];
+    
+    foreach($meta->AffectedNodes as $affected_node) {
+
+      if(isset($affected_node->ModifiedNode)) {
+        if($affected_node->ModifiedNode->LedgerEntryType === 'NFTokenPage') {
+          $inout = $this->extractNFTokenIDsFromNFTTokenPageChange(
+            $affected_node->ModifiedNode->PreviousFields,
+            $affected_node->ModifiedNode->FinalFields
+          );
+          $in = \array_merge($in,$inout['in']);
+          $out = \array_merge($out,$inout['out']);
+        }
+      }
+    }
+
+    $in = \array_unique($in);
+    $out = \array_unique($out);
+    //dd($in,$out,'exit');
+    $this->result_in = $in;
+    $this->result_out = $out;
+  }
+
+  /**
+   * @return array ['in' => [ 'NFTokenID, ... ], 'out' => [ 'NFTokenID, ... ] ]
+   */
+  private function extractNFTokenIDsFromNFTTokenPageChange($PreviousFields, $FinalFields): array
+  {
+    $in = $out = [];
+    $prev_tokens  = isset($PreviousFields->NFTokens) ? $PreviousFields->NFTokens : [];
+    $final_tokens = isset($PreviousFields->NFTokens) ? $FinalFields->NFTokens : [];
+
+    $prev = [];
+    foreach($prev_tokens as $pt) {
+      $prev[] = $pt->NFToken->NFTokenID;
+    }
+
+    $final = [];
+    foreach($final_tokens as $ft) {
+      $final[] = $ft->NFToken->NFTokenID;
+    }
+
+    $in = \array_diff($final,$prev);
+    $out = \array_diff($prev,$final);
+    return ['in' => \array_values($in), 'out' => \array_values($out)];
+  }
+
+  public function result(): array
+  {
+    return [
+      'in' => $this->result_in,
+      'out' => $this->result_out
+    ];
+  }
 }
--- tests/Tx1Test.php
+++ tests/Tx1Test.php
@@ -1,1 +1,81 @@
-.
+<?php declare(strict_types=1);
+
+namespace XRPLWin\XRPLNFTTxMutatationParser\Tests;
+
+use PHPUnit\Framework\TestCase;
+use XRPLWin\XRPLNFTTxMutatationParser\NFTTxMutationParser;
+
+/***
+ * @see https://hash.xrp.fans/D904ADB2D6DD9644B7ACC14E351536B8570F8451AAB01E946ADB47B1E381399F/json
+ */
+final class Tx1Test extends TestCase
+{
+    public function testNFTokenMintList()
+    {
+        $transaction = file_get_contents(__DIR__.'/fixtures/tx1.json');
+        $transaction = \json_decode($transaction);
+        $account = "rHeRoYtbiMSKhtXm4k7tff1PrcwYnCePR3";
+        $NFTTxMutationParser = new NFTTxMutationParser($account, $transaction->result);
+        $parsedTransaction = $NFTTxMutationParser->result();
+        dd($parsedTransaction);
+        //Self (own account) must be $account
+        $this->assertEquals($account,$parsedTransaction['self']['account']);
+
+        # Basic info
+
+        //Own account: two balance changes
+        $this->assertEquals(2,count($parsedTransaction['self']['balanceChanges']));
+
+        //Transaction type TRADE
+        $this->assertEquals(TxMutationParser::MUTATIONTYPE_TRADE,$parsedTransaction['type']);
+
+        $this->assertFalse($parsedTransaction['self']['feePayer']);
+
+        # Event list
+
+        //contains (correct) `primary` entry
+        $this->assertArrayHasKey('primary',$parsedTransaction['eventList']);
+        $this->assertEquals([
+            'counterparty' => 'rCSCManTZ8ME9EoLrSHHYKW8PPwWMgkwr',
+            'currency' => 'CSC',
+            'value' => '1.999999999999'
+        ],$parsedTransaction['eventList']['primary']);
+
+        //contains (correct) `secondary` entry
+        $this->assertArrayHasKey('secondary',$parsedTransaction['eventList']);
+        $this->assertEquals([
+            'currency' => 'XRP',
+            'value' => '-0.004362'
+        ],$parsedTransaction['eventList']['secondary']);
+
+        # Event flow
+
+        //contains (correct) `start` entry
+        $this->assertArrayHasKey('start',$parsedTransaction['eventFlow']);
+        $this->assertArrayHasKey('account',$parsedTransaction['eventFlow']['start']);
+        $this->assertEquals('rogue5HnPRSszD9CWGSUz8UGHMVwSSKF6',$parsedTransaction['eventFlow']['start']['account']);
+
+        //contains (correct) `intermediate` entry
+        $this->assertArrayHasKey('intermediate',$parsedTransaction['eventFlow']);
+        $this->assertEquals([
+            'account' => $account,
+            'mutations' => [
+                'in' => [
+                    'counterparty' => "rCSCManTZ8ME9EoLrSHHYKW8PPwWMgkwr",
+                    'currency' => "CSC",
+                    'value' => "1.999999999999",
+                ],
+                'out' => [
+                    'currency' => "XRP",
+                    'value' => "-0.004362",
+                ]
+            ]
+        ],$parsedTransaction['eventFlow']['intermediate']);
+
+        //contains (correct) `end` entry
+        $this->assertArrayHasKey('end',$parsedTransaction['eventFlow']);
+        $this->assertArrayHasKey('account',$parsedTransaction['eventFlow']['end']);
+        $this->assertEquals('rogue5HnPRSszD9CWGSUz8UGHMVwSSKF6',$parsedTransaction['eventFlow']['end']['account']);
+
+    }
+}
